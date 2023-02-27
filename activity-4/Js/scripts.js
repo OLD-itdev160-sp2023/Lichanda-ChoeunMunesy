35aa0054a86164b9e2f7b71620aa9de46e5088eb
LannaChoeun <124404244+LannaChoeun@users.noreply.github.com>
2023-02-26 18:02:14 -0600

    Add files via upload

---
+++ activity-4/Js/scripts.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  var user = 'Lanna';
+var salutation = 'Hello, ';
+var greeting = salutation + user + '! Here are the latest HP Omen PC Reviews.';
+var greetingEl = document.getElementById('greeting');
+
+greetingEl.textContent = greeting;
+
+var price = 1000,
+studentDiscount = 0.10,
+studentPrice = price - (price * studentDiscount),
+priceEl =  document.getElementById('price'),
+studentPriceEl = document.getElementById('student-price');
+
+priceEl.textContent = price.toFixed(2);
+studentPriceEl.textContent = studentPrice.toFixed(2);
+ 
+};
+
+
